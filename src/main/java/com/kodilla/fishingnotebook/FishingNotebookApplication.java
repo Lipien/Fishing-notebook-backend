3765c890a68ff9e087c5c0c4d0c80d982908ee4e
Lipien <adam@pawtel.pl>
2019-05-27 09:46:49 +0200

    Initial commit.

---
+++ src/main/java/com/kodilla/fishingnotebook/FishingNotebookApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.fishingnotebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishingNotebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishingNotebookApplication.class, args);
+	}
+
+}
