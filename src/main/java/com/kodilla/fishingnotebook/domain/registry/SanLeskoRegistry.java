76375146c82eca8dd261837a5876f5fc994aa3aa
Lipien <adam@pawtel.pl>
2019-06-19 15:35:55 +0200

    Minor fixes and code cleanup.

--- src/main/java/com/kodilla/fishingnotebook/domain/registry/SanLeskoRegistry.java
+++ src/main/java/com/kodilla/fishingnotebook/domain/registry/SanLeskoRegistry.java
@@ -5,8 +5,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import javax.transaction.Transactional;
-import java.time.LocalDate;
 
 @AllArgsConstructor
 @NoArgsConstructor
